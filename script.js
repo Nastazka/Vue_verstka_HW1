094fc8a84de41fe90f03e30b26db329553d62feb
Anastasia <nastazka8@gmail.com>
2024-03-22 11:48:56 +0300

    script file

---
+++ script.js
@@ -0,0 +1,45 @@
+const articlesData = [
+    {
+        id: 1,
+        title: "Let’s Get Solution For Building Construction Work",
+        image: "./img/Article1.png",
+        date: "26 December,2022",
+    },
+    {
+        id: 2,
+        title: "Low Cost Latest Invented Interior Designing Ideas.",
+        image: "./img/Article2.png",
+        date: "22 December,2022",
+    },
+    {
+        id: 3,
+        title: "Best For Any Office & Business Interior Solution",
+        image: "./img/Article3.png",
+        date: "25 December,2022",
+    },
+    {
+        id: 4,
+        title: "Let’s Get Solution For Building Construction Work",
+        image: "./img/Article4.png",
+        date: "26 December,2022",
+    },
+    {
+        id: 5,
+        title: "Low Cost Latest Invented Interior Designing Ideas.",
+        image: "./img/Article5.png",
+        date: "22 December,2022",
+    },
+    {
+        id: 6,
+        title: "Best For Any Office & Business Interior Solution",
+        image: "./img/Article6.png",
+        date: "25 December,2022",
+    },
+];
+
+new Vue({
+    el: '#articles-news',
+    data: {
+        articlesData: articlesData,
+    },
+});
